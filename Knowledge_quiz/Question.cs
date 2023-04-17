0d471ed7705c46dcad7f65c9d6eaf8db7815f4c6
Sashok9203 <56803757+Sashok9203@users.noreply.github.com>
2023-04-17 20:18:07 +0300

    Update

--- Knowledge_quiz/Question.cs
+++ Knowledge_quiz/Question.cs
@@ -1,12 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace KnowledgeQuiz
 {
-    internal class Question
+    internal abstract class Question
     {
+		protected string _question;
+
+		public string QuestionString
+        {
+			get  => _question;
+			set 
+			{
+				if (string.IsNullOrWhiteSpace(value)) throw new ApplicationException("Текст питання не може бути пустим...");
+				_question = value;
+			}
+		}
+
+		protected Question() { }
     }
 }
